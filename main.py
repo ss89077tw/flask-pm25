60b02831a145365c36075166c8a9dc4c3c605208
sincheng <ss89077tw@gmail.com>
2021-10-07 22:46:14 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,72 @@
+from flask import Flask
+from datetime import datetime
+
+from flask.templating import render_template
+from scrape.pm25 import get_pm25
+
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    name = 'Jerry'
+    time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    return render_template('index.html', **locals())
+
+
+@app.route('/echarts')
+def echarts():
+    return render_template('echarts.html', time=time)
+
+
+@app.route('/pm25')
+def pm25():
+    time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    columns, datas = get_pm25()
+    return render_template('pm25.html', **locals())
+
+
+@app.route('/stock')
+def getStock():
+    return render_template('stock.html', time=time, stocks=stocks)
+
+
+if __name__ == '__main__':
+
+    time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    stocks = [
+        {'分類': '日經指數', '指數': '22,920.30'},
+        {'分類': '韓國綜合', '指數': '2,304.59'},
+        {'分類': '香港恆生', '指數': '25,083.71'},
+        {'分類': '上海綜合', '指數': '3,380.68'}
+    ]
+    # for stock in stocks:
+    #     print(stock['分類'], stock['指數'])
+
+    # return render_template('stock.html',**locals())
+
+
+@app.route('/test')
+def test():
+    return render_template('index.html')
+
+# Get 取值
+
+
+@app.route('/sum/x=<x>&y=<y>', methods=['GET'])
+def get_Sum(x, y):
+    return f'總和為{x+y}'
+
+
+@app.route('/today/<string:name>')
+def getTodayDate(name):
+    from datetime import datetime
+    print(datetime.now())
+    # 字串
+    return f'{name} 歡迎光臨<br/> {datetime.now().strftime("%Y-%m-%d %H:%M:%S")}'
+    return datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
