f0207f7c8435db60966644c710daf4029aa483d1
sincheng <ss89077tw@gmail.com>
2021-10-11 18:22:21 +0800

    commit

--- static/pm25-echarts.js
+++ static/pm25-echarts.js
@@ -6,11 +6,11 @@ console.log(charts)
 //指定圖表的配置項和資料
 const option = {
     title: {
-        text: "echart圖形繪製DEMO",
+        text: "PM2.5圖形繪製",
     },
     tooltip: {},
     legend: {
-        data: ["銷量"],
+        data: ["PM2.5"],
     },
     xAxis: {
         data: ["襯衫", "羊毛衫", "雪紡衫", "褲子", "高跟鞋", "襪子"],
@@ -18,7 +18,7 @@ const option = {
     yAxis: {},
     series: [
         {
-            name: "銷量",
+            name: "PM2.5",
             type: "bar",
             data: [5, 20, 36, 10, 10, 20],
         },
