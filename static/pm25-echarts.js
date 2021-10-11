2744bed8ad432667182452ff9290452f297311d6
sincheng <ss89077tw@gmail.com>
2021-10-11 23:02:33 +0800

    commit

--- static/pm25-echarts.js
+++ static/pm25-echarts.js
@@ -3,26 +3,43 @@ console.log(mainEl);
 let charts = echarts.init(mainEl);
 console.log(charts)
 
-//指定圖表的配置項和資料
-const option = {
-    title: {
-        text: "PM2.5圖形繪製",
-    },
-    tooltip: {},
-    legend: {
-        data: ["PM2.5"],
-    },
-    xAxis: {
-        data: ["襯衫", "羊毛衫", "雪紡衫", "褲子", "高跟鞋", "襪子"],
-    },
-    yAxis: {},
-    series: [
-        {
-            name: "PM2.5",
-            type: "bar",
-            data: [5, 20, 36, 10, 10, 20],
-        },
-    ],
-};
 
-charts.setOption(option)
+$(document).ready(() => {
+    $.ajax(
+        {
+            url: "/pm25-data",
+            type: "POST",
+            datetype: "json",
+            success:(datas)=>{
+                //指定圖表的配置項和資料
+                const option = {
+                    title: {
+                        text: "PM2.5圖形繪製",
+                    },
+                    tooltip: {},
+                    legend: {
+                        data: ["PM2.5"],
+                    },
+                    xAxis: {
+                        data: datas['sites'],
+                    },
+                    yAxis: {},
+                    series: [
+                        {
+                            name: "PM2.5",
+                            type: "bar",
+                            data: datas['values'],
+                        },
+                    ],
+                };
+                
+                charts.setOption(option)
+
+            }
+        }
+    )    
+});   
+
+    
+
+
